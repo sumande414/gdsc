bc82ad387491d0040f94d59c8766bfdafe6ce46c
Suman De <sumande0414@gmail.com>
2023-08-28 10:18:23 +0530

    add material you system wide theme colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gdsc/home_page.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 
 void main() {
   runApp(const MainApp());
@@ -8,16 +9,30 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  static final _defaultLightColorScheme =
+      ColorScheme.fromSwatch(primarySwatch: Colors.purple);
+
+  static final _defaultDarkColorScheme = ColorScheme.fromSwatch(
+      primarySwatch: Colors.purple, brightness: Brightness.dark);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      darkTheme: ThemeData.dark(useMaterial3: true),
-      title: "Conferences",
-      home: Scaffold(
-        appBar: AppBar(title:const Text("Conferences"),),
-        body: const HomePage(),
-      ),
-    );
+    return DynamicColorBuilder(builder: (lightColorScheme, darkColorScheme) {
+      return MaterialApp(
+        theme: ThemeData(
+            colorScheme: lightColorScheme ?? _defaultLightColorScheme,
+            useMaterial3: true),
+        darkTheme: ThemeData(
+            colorScheme: darkColorScheme ?? _defaultDarkColorScheme,
+            useMaterial3: true),
+        title: "Conferences",
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Conferences"),
+          ),
+          body: const HomePage(),
+        ),
+      );
+    });
   }
 }
